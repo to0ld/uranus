cc8f67d28389bf4c2d2f7bab500b16886c31329b
to0ld <to0ld@qq.com>
2015-03-17 01:34:24 +0800

    提供批量插入的保护排重

---
+++ common/src/main/java/net/popbean/pf/persistence/vo/BatchVO.java
@@ -0,0 +1,16 @@
+package net.popbean.pf.persistence.vo;
+
+import net.popbean.pf.entity.IValueObject;
+import net.popbean.pf.entity.field.Domain;
+import net.popbean.pf.entity.field.annotation.Entity;
+import net.popbean.pf.entity.field.annotation.Field;
+@Entity(code="pb_pf_batch")
+public class BatchVO implements IValueObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1396210999336946036L;
+	@Field(domain=Domain.PK)
+	public String id;
+}

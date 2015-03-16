bc2a5ec93b07c33726b135078138c197744fc28b
to0ld <to0ld@qq.com>
2015-03-17 01:26:59 +0800

    规范实体的命名

--- common/src/main/java/net/popbean/pf/entity/model/EntityModel.java
+++ common/src/main/java/net/popbean/pf/entity/model/EntityModel.java
@@ -1,12 +1,11 @@
 package net.popbean.pf.entity.model;
 
-import java.io.Serializable;
 import java.util.List;
 
-import net.popbean.pf.entity.IValueObject;
 import net.popbean.pf.entity.field.Domain;
 import net.popbean.pf.entity.field.annotation.Entity;
 import net.popbean.pf.entity.field.annotation.Field;
+import net.popbean.pf.entity.impl.AbstractValueObject;
 import net.popbean.pf.entity.model.helper.EntityModelHelper;
 
 /**
@@ -17,22 +16,22 @@ import net.popbean.pf.entity.model.helper.EntityModelHelper;
  *
  */
 @Entity(code="pb_pf_entity")
-public class EntityModel implements Serializable, Cloneable,IValueObject {
+public class EntityModel extends AbstractValueObject {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4365726099154277092L;
-	@Field(domain=Domain.PK)
-	public String pk_entity;
 	@Field(domain=Domain.Code)
 	public String code;//FIXME 对于分表的情况而言，还是要处理一下的(暂时不支持分表的情况吧)
 	@Field(domain=Domain.Code)
 	public String name;
 	@Field(domain=Domain.Memo,required=false)
 	public String memo;
+	@Field
 	public EntityType type = EntityType.Normal;
 	//
+	@Field
 	public String clazz;//其实我不想留这个字段，没有它用code一样可以，因为表名是唯一的
 	//
 	public List<FieldModel> field_list;

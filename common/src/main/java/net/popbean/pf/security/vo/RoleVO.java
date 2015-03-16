9f9d10f560de32c7802848abeffa111bbb1b237d
to0ld <to0ld@qq.com>
2015-03-17 01:28:44 +0800

    规范实体命名

--- common/src/main/java/net/popbean/pf/security/vo/RoleVO.java
+++ common/src/main/java/net/popbean/pf/security/vo/RoleVO.java
@@ -1,10 +1,28 @@
 package net.popbean.pf.security.vo;
 
+import net.popbean.pf.entity.field.Domain;
+import net.popbean.pf.entity.field.annotation.Entity;
+import net.popbean.pf.entity.field.annotation.Field;
 import net.popbean.pf.entity.impl.AbstractValueObject;
 
+@Entity(code = "pb_bd_role", name = "角色信息")
 public class RoleVO extends AbstractValueObject {
-	public String id;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3918195315421352204L;
+	@Field
 	public String code;
+	@Field
 	public String name;
+	@Field(domain = Domain.Ref)
+	public String account_crt_ref;
+	@Field(domain = Domain.Ref)
+	public String company_crt_ref;
+	@Field(domain = Domain.Memo)
 	public String memo;
+	@Field(name = "应用编码")
+	public String app_code;
+
 }

2c724d954b7d6f76b06939e592d4c9a123604c49
to0ld <to0ld@qq.com>
2015-03-20 00:11:34 +0800

    下定决心还是不搞奇葩的ref(pk+name)还是分开两个字段吧

--- common/src/main/java/net/popbean/pf/entity/field/Domain.java
+++ common/src/main/java/net/popbean/pf/entity/field/Domain.java
@@ -9,7 +9,7 @@ package net.popbean.pf.entity.field;
  *
  */
 public enum Domain{
-	PK(60), Code(60), Date(5), TimeStamp(7), Memo(1000), Money(12),Seriescode(320),Stat(19),Clob(23),Int(29),Ref(120);
+	PK(60), Code(60), Date(5), TimeStamp(7), Memo(1000), Money(12),Seriescode(320),Stat(19),Clob(23),Int(29),Ref(60);
 	int _value;
 	private Domain(int value){
 		this._value = value;

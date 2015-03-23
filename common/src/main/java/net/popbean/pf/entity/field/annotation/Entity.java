62a4cc01b49d5a6636e8e95b59b610e0a7745777
to0ld <to0ld@qq.com>
2015-03-23 19:26:38 +0800

    确保可以被继承

--- common/src/main/java/net/popbean/pf/entity/field/annotation/Entity.java
+++ common/src/main/java/net/popbean/pf/entity/field/annotation/Entity.java
@@ -2,6 +2,7 @@ package net.popbean.pf.entity.field.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -13,6 +14,7 @@ import net.popbean.pf.entity.model.EntityType;
  *
  */
 @Documented
+@Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Entity {

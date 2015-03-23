10a0800e983c3af19bdbcaaffb43916f6e1a302c
to0ld <to0ld@qq.com>
2015-03-23 19:33:10 +0800

    清除不必要的import

--- bill/src/test/java/testcase/bill/BillModelTestcase.java
+++ bill/src/test/java/testcase/bill/BillModelTestcase.java
@@ -22,7 +22,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import testcase.bill.vo.MasterVO;
-import testcase.business.BusinessServiceTestCase.TestVO;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;

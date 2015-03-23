60c5ee03fa94800ebaa70761dfa2308d1b5a9369
to0ld <to0ld@qq.com>
2015-03-23 19:31:57 +0800

    统一标准属性的命名

--- common/src/main/java/net/popbean/pf/security/vo/CompanyVO.java
+++ common/src/main/java/net/popbean/pf/security/vo/CompanyVO.java
@@ -18,9 +18,9 @@ public class CompanyVO implements IValueObject {
 	private static final long serialVersionUID = 4168320084899460389L;
 	//
 	@Field(name="企业主键",domain=Domain.PK)
-	public String company_id;
+	public String id;
 	@Field(name="企业编码",domain=Domain.Code)
-	public String company_code;
+	public String code;
 	@Field(name="企业名称",domain=Domain.Code)
-	public String company_name;
+	public String name;
 }

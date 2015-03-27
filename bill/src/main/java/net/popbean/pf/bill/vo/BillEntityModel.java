895aa7eabd95125a2a1ffd7010116fd0f6b02ada
to0ld <to0ld@qq.com>
2015-03-27 20:06:37 +0800

    增强一下功能，确保写起来顺当一点

--- bill/src/main/java/net/popbean/pf/bill/vo/BillEntityModel.java
+++ bill/src/main/java/net/popbean/pf/bill/vo/BillEntityModel.java
@@ -2,6 +2,7 @@ package net.popbean.pf.bill.vo;
 
 import java.util.List;
 
+import net.popbean.pf.bill.helpers.BillModelHelper;
 import net.popbean.pf.entity.impl.AbstractValueObject;
 
 /**
@@ -16,8 +17,15 @@ public class BillEntityModel extends AbstractValueObject{
 	 */
 	private static final long serialVersionUID = 1166654519700500124L;
 	//
+	public static final String REF_TYPE_NONE = "none";
+	public static final String REF_TYPE_WEAK = "weak";
+	//
 	public String code;
 	public String name;
+	public String ref_type = REF_TYPE_NONE;
 	public List<BillFieldModel> fields;
 	public int column = 1;//默认一行一个控件，咱就别layout了
+	public BillFieldModel findPk(){
+		return BillModelHelper.findPK(this.fields);
+	}
 }

6797b717e2ec8643a7c59c78017f6b48e47f8278
to0ld <to0ld@qq.com>
2015-03-27 20:26:49 +0800

    完善关系模型

--- common/src/main/java/net/popbean/pf/entity/model/RelationModel.java
+++ common/src/main/java/net/popbean/pf/entity/model/RelationModel.java
@@ -1,6 +1,8 @@
 package net.popbean.pf.entity.model;
 
+import net.popbean.pf.entity.field.Domain;
 import net.popbean.pf.entity.field.annotation.Entity;
+import net.popbean.pf.entity.field.annotation.Field;
 import net.popbean.pf.entity.impl.AbstractValueObject;
 /**
  * 
@@ -14,10 +16,21 @@ public class RelationModel extends AbstractValueObject {
 	 */
 	private static final long serialVersionUID = 735854453658840266L;
 	//
+	//
+	public static final String TYPE_INNER = "inner";
+	public static final String TYPE_OUTER = "outer";
+	public static final String TYPE_BRIDGE = "bridge";
+	//
+	@Field
 	public String code;//桥接表自身，也可以叫bridge_code;
+	@Field
 	public String entity_code_main;
+	@Field(domain=Domain.Ref)
 	public String id_key_main;
+	@Field
 	public String entity_code_slave;
+	@Field(domain=Domain.Ref)
 	public String id_key_slave;
-	public String type;//关系类型
+	@Field
+	public String type;//关系类型:inner:outer；bridge
 }

8cd99c3de1778017d3ae001d8c9d7571f3b5f797
to0ld <to0ld@qq.com>
2015-03-27 20:26:00 +0800

    确保主键生成的策略更灵活

--- common/src/main/java/net/popbean/pf/id/helper/IdGenHelper.java
+++ common/src/main/java/net/popbean/pf/id/helper/IdGenHelper.java
@@ -5,12 +5,19 @@ import org.apache.commons.lang3.StringUtils;
 import net.popbean.pf.exception.BusinessError;
 
 public class IdGenHelper {
-	public static String genID(String entity_code,String field_code)throws BusinessError{
+	public static String genID(String... keys)throws BusinessError{
 		//主键格式为：表名.字段名.补零
-		StringBuilder ret = new StringBuilder(entity_code);
-
-		if(!StringUtils.isBlank(field_code)){
-			ret.append(":").append(field_code);//大家一致认为这个比较合适
+		StringBuilder ret = new StringBuilder();
+		//
+		int pos = 0;
+		for(String key:keys){
+			if(pos!=0){
+				ret.append(":");
+			}
+			if(!StringUtils.isBlank(key)){
+				ret.append(key);
+				pos++;
+			}
 		}
 		return ret.toString();
 	}

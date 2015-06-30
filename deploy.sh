5c215b460e9c79bd2436e32f0f4e81b0e965483d
to0ld@qq.com <to0ld@qq.com>
2015-06-30 11:26:15 +0800

    osc的maven服务挂了谴责一下，现迁移到maven central中

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 if [ -x /usr/libexec/java_home ]; then
 	export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 fi
-mvn clean deploy -s settings_osc.xml
+mvn clean deploy -DskipTests=true -s settings_central.xml

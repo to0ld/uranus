88f26a2f81e411d003f5b9a87b393c58d71907c4
to0ld@qq.com <to0ld@qq.com>
2015-07-19 00:46:38 +0800

    明确一下用户与组织的关系

--- security/src/main/java/net/popbean/pf/security/vo/RltOrgAccount.java
+++ security/src/main/java/net/popbean/pf/security/vo/RltOrgAccount.java
@@ -7,7 +7,7 @@ import net.popbean.pf.entity.impl.AbstractValueObject;
 
 /**
  * 组织机构-账号，映射关系 仅仅适用于业务过程
- * 
+ * 需要注意的是，一定是先注册用户，然后再登录创建企业
  * @author to0ld
  *
  */
@@ -25,4 +25,6 @@ public class RltOrgAccount extends AbstractValueObject {
 	public String account_id;
 	@Field(domain = Domain.Stat, name = "最新状态")
 	public int last;
+	@Field(domain = Domain.Stat, name = "用户类型")
+	public int type = 0;//0:普通用户；3：企业管理员
 }

4326bd030b7354677bfc5adf8022ca163cb0bb03
to0ld@qq.com <to0ld@qq.com>
2015-08-02 19:12:36 +0800

    为了方便fe的使用，domain采用全小写

--- bill/src/main/java/net/popbean/pf/bill/vo/BillFieldModel.java
+++ bill/src/main/java/net/popbean/pf/bill/vo/BillFieldModel.java
@@ -16,13 +16,14 @@ public class BillFieldModel extends AbstractValueObject{
 	private static final long serialVersionUID = 1L;
 	public String code;//属性编码
 	public String name;//算是label吧
-	public Domain domain = Domain.Code;//默认是code
+	public Domain type = Domain.code;//默认是code
 	public String tpl;//显示模板,最好是前(js)后(java)都能解释，你看 ${code}这个就挺好
 	public String def_value;//默认值：支持时间戳；登录变量,如果不嫌弃就先采用spel反正前台也不需要？。。。这是谬论，点击两次新增，难道都从后台取数据？
 	public boolean showcard = true;//卡片状态显示
 	public boolean showlist = true;//列表状态显示
 	public int cardpos = 0;//卡片位置
 	public int listpos = 0;
+	public Scope scope= Scope.data;//默认是数据
 	//
 	public boolean required = true;
 	public boolean readonly = true;//确实有必要增加这一项，确保其不受editable控制
